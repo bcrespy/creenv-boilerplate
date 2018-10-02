dbf80c885666180386696e70a2ccd42d62bccb42
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-02 17:30:27 +0200

    small starter to see if everything is working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { Test } from './test';
+import { Creenv } from '@creenv/core';
+import Canvas from '@creenv/canvas';
 
 
-let test = new Test();
-test.write();
+class MyProject extends Creenv {
+  init() {
+    this.x = 0;
+
+    return new Promise( (resolve, reject) => {
+      this.cvs = new Canvas();
+      resolve();
+    });
+  }
+
+  render() {
+    this.cvs.fillStyle("blue");
+    this.cvs.rect(this.x,50,40,40);
+    this.cvs.fillStyle("red");
+    this.cvs.context.fillRect( this.x, 10, 50, 20 );
+    if (this.frames%2 === 0) {
+      this.x++;
+    }
+  }
+}
+
+
+let project = new MyProject();
+project.bootstrap();
--- src/test.js
+++
@@ -1,5 +0,0 @@
-export class Test {
-  write() {
-    console.log("this is a test");
-  }
-}
