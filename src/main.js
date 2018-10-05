3204999d18bc2240a9bf906cf1ab3350a2c9dd39
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-05 15:44:54 +0200

    updated the example provided with the boilerplate

--- src/main.js
+++ src/main.js
@@ -1,33 +1,65 @@
-import { Creenv } from '@creenv/core';
+/**
+ * This really simple project show you how you can use Creenv to create 
+ * creative applications. It focuses on the core of Creenv to show you how 
+ * simple it is to work with Creenv. However, this project only covers a 
+ * small part of all the things you can do with Creenv. I strongly encourage 
+ * that you take a look at all the examples provided here 
+ * 
+ * [needs_to_be_added]
+ * 
+ * Cochon farci, fromage, et pomme de pin
+ */
+
+
+import Creenv from '@creenv/core';
 import Canvas from '@creenv/canvas';
-import test from './image.jpeg';
 
-let toto = new Image();
-toto.onload = () => { document.body.appendChild(toto) }
-toto.src = test;
+/**
+ * For the sake of the example, the rendering logic will take part 
+ * in the render file
+ * You should aways split a bit your code, it will make the process of
+ * identifying mistakes and improving your app easier :)
+ */
+import Renderer from './renderer';
 
 
+/**
+ * The main class can inherit the Core of creenv
+ * As you can see, this simplifies the process of setting up your 
+ * application 
+ */
 class MyProject extends Creenv {
+  /**
+   * The init method will automatically be called by the bootstrap method,
+   * and the end of the file. This is where you can fetch files (return a 
+   * Promise in such a case, bootstrap will handle it automatically), 
+   * allocate your data... etc
+   */
   init() {
-    this.x = 0;
+    super.init(); // REQUIRED - calls the parent method 
 
-    return new Promise( (resolve, reject) => {
-      this.cvs = new Canvas();
-      resolve();
-    });
+    // optional, it just show that you can set the framerate like that
+    super.framerate(60);
+
+    // for the example
+    this.renderer = new Renderer();
+    this.renderer.init();
   }
 
+  /**
+   * This function will be called at each frame, automatically, given 
+   * the framerate, after the bootstrap method is called
+   */
   render() {
-    this.cvs.fillStyle("blue");
-    this.cvs.rect(this.x,50,40,40);
-    this.cvs.fillStyle("red");
-    this.cvs.context.fillRect( this.x, 10, 50, 20 );
-    if (this.frames%2 === 0) {
-      this.x++;
-    }
+    // for the example
+    this.renderer.render(this.elapsedTime);
   }
 }
 
 
+/**
+ * You instanciate your class and then call the bootstrap method.
+ * This will start the rendering process
+ */
 let project = new MyProject();
-project.bootstrap();
+project.bootstrap(); 
